02c79d44fb9dcb94be1174325da049b272721b9e
joer0k <ryabokon.niki08@gmail.com>
2025-03-26 17:11:44 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 
 from utils import get_static_api_image
@@ -11,19 +12,26 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.map_ll = [38.913250, 45.038852]
-        self.spn = 0.005
+        self.z = 17
         uic.loadUi('untitled.ui', self)  # Загружаем дизайн
         self.refresh_map()
 
     def refresh_map(self):
-        response = get_static_api_image(self.map_ll, spn=[self.spn, self.spn])
-        # print(response)
+        response = get_static_api_image(self.map_ll, z=self.z)
         if response:
             with open('image.png', mode='wb') as file:
                 file.write(response)
             pixmap = QPixmap()
             pixmap.load('image.png')
             self.label.setPixmap(pixmap)
+            self.label.setScaledContents(True)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            self.z = min(self.z + 1, 21)
+        if event.key() == Qt.Key.Key_PageDown:
+            self.z = max(self.z - 1, 0)
+        self.refresh_map()
 
 
 if __name__ == '__main__':
