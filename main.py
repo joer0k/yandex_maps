c85f7f167839f83f16d1819aa0129bc518bcce53
joer0k <ryabokon.niki08@gmail.com>
2025-03-27 11:23:00 +0300

    Большая задача по Maps API. Часть №5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QMainWindow, QApplication
 
-from utils import get_static_api_image
+from utils import get_static_api_image, get_ll
 
 
 class MyWidget(QMainWindow):
@@ -14,15 +14,18 @@ class MyWidget(QMainWindow):
         super().__init__()
         self.map_ll = [38.913250, 45.038852]
         self.z = 10
+        self.points = set()
         uic.loadUi('untitled.ui', self)  # Загружаем дизайн
         self.type_map = 'light'
         self.btn_light.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.btn_dark.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.btn_light.toggled.connect(self.change_theme)
+        self.btn_search.clicked.connect(self.search)
         self.refresh_map()
 
     def refresh_map(self):
-        response = get_static_api_image(self.map_ll, z=self.z, size=[self.width(), self.height()], theme=self.type_map)
+        response = get_static_api_image(self.map_ll, z=self.z, size=[self.width(), self.height()], theme=self.type_map,
+                                        points=self.points)
         if response:
             with open('image.png', mode='wb') as file:
                 file.write(response)
@@ -39,6 +42,20 @@ class MyWidget(QMainWindow):
             self.type_map = 'dark'
         self.refresh_map()
 
+    def search(self):
+        data = self.edit_search.text()
+        if data.strip(' ') != '':
+            res = get_ll(data)
+            if res:
+                self.map_ll = list(map(float, res['Point']['pos'].split()))
+                self.points.add(','.join(map(str, res['Point']['pos'].split())))
+                self.label_result.setText('')
+                self.refresh_map()
+            else:
+                self.label_result.setText('Ничего не найдено, попробуйте снова')
+        else:
+            self.label_result.setText('Неверный запрос')
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_PageUp:
             self.z = min(self.z + 1, 21)
@@ -52,10 +69,17 @@ class MyWidget(QMainWindow):
             self.map_ll[1] += self.z / 100
         if event.key() == Qt.Key.Key_Down:
             self.map_ll[1] -= self.z / 100
+        if event.key() == Qt.Key.Key_Enter:
+            self.search()
         self.refresh_map()
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
+    sys.excepthook = except_hook
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
--- utils.py
+++ utils.py
@@ -2,17 +2,19 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
-API_KEY = '371e1360-5397-45a5-9639-2e7da34bd060'
+API_KEY_ST = '371e1360-5397-45a5-9639-2e7da34bd060'
+API_KEY_GEO = 'd2f5711d-9e67-414c-aa2c-d7c0465aea3e'
 
 
-def get_static_api_image(ll, z, size, theme):
+def get_static_api_image(ll, z, size, theme, points):
     server = 'https://static-maps.yandex.ru/v1?'
     map_params = {
         'll': ','.join(map(str, ll)),
-        'apikey': API_KEY,
+        'apikey': API_KEY_ST,
         'z': z,
         'size:': ','.join(map(str, size)),
-        'theme': theme
+        'theme': theme,
+        'pt': '~'.join(points)
     }
 
     session = requests.Session()
@@ -22,5 +24,23 @@ def get_static_api_image(ll, z, size, theme):
     response = session.get(server, params=map_params)
     if response:
         return response.content
+    else:
+        return None
+
+
+def get_ll(address):
+    server = 'http://geocode-maps.yandex.ru/1.x/?'
+    map_params = {
+        'apikey': API_KEY_GEO,
+        'geocode': address,
+        'format': 'json',
+    }
+    session = requests.Session()
+    retry = Retry(total=10, connect=5, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    response = session.get(server, params=map_params)
+    if response:
+        return response.json()["response"]["GeoObjectCollection"]["featureMember"][0]['GeoObject']
     else:
         return None
